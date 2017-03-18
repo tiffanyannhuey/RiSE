0b0a6b724e00705c3197ddfd2634ec368866ac42
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-03-18 16:43:03 -0700

    add custom index cell controller

---
+++ Wakeupz/CustomIndexCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomIndexCell.swift
+//  Wakeupz
+//
+//  Created by Tiffany Huey on 3/18/17.
+//  Copyright © 2017 F4. All rights reserved.
+//
+
+import UIKit
+
+class CustomIndexCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
