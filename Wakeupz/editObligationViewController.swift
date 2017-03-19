6ba591db0b6c6b7b49f681983c83d19f0ed34e4d
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-03-19 12:53:57 -0700

    add rest of controllers besides create

---
+++ Wakeupz/editObligationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  editObligationViewController.swift
+//  Wakeupz
+//
+//  Created by Tiffany Huey on 3/19/17.
+//  Copyright © 2017 F4. All rights reserved.
+//
+
+import UIKit
+
+class editObligationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
