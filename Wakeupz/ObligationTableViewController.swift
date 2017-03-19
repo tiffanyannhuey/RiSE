1960be2137ea991637e1093af19dffac52826a0c
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-03-18 20:08:24 -0700

    add cell outlets to controller

--- Wakeupz/ObligationTableViewController.swift
+++ Wakeupz/ObligationTableViewController.swift
@@ -11,19 +11,21 @@ import UIKit
 class ObligationTableViewController: UITableViewController {
     
     var places = ["DBC", "Home", "Work", "Gym", "Church" ]
+    var addresses = ["707 Broadway Ave | San Diego, CA | 92101", "707 Broadway Ave | San Diego, CA | 92101", "707 Broadway Ave | San Diego, CA | 92101", "707 Broadway Ave | San Diego, CA | 92101", "707 Broadway Ave | San Diego, CA | 92101"]
 
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return places.count
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath) as! CustomRouteCell
+        
+        cell.locationName.text = places[indexPath.row]
+        cell.locationAddress.text = addresses[indexPath.row]
 
         return cell
     }
