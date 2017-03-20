2ca06189546bfcdf873ed7373b3993288504c546
allyshsaye <allisonshaye@gmail.com>
2017-03-20 11:59:33 -0700

    Update Create Alarm controller to display existing obligations

--- Wakeupz/createAlarmViewController.swift
+++ Wakeupz/createAlarmViewController.swift
@@ -18,26 +18,48 @@ class createAlarmViewController: UIViewController, UIPickerViewDelegate, UIPicke
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var isOn: UISwitch!
 
+    var obligations: [Obligation] = []
     
-    var places = ["DBC", "Home", "Work", "Gym", "Church" ]
-    var placeSelection = 0
+    // var places = ["DBC", "Home", "Work", "Gym", "Church" ]
+    // var placeSelection = 0
+    var obligationSelection = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        picker.dataSource = self
+        picker.delegate = self
 
         earliestWakeupTime.backgroundColor = .white
         
     }
     
+    func getData() {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            obligations = try context.fetch(Obligation.fetchRequest())
+        } catch {
+            print("Fetching failed")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getData()
+        // picker.reloadData()
+    }
     
     //Setup for location picker 
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return places[row]
+        // return places[row]
+        let obligation = obligations[row]
+        return obligation.name
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return places.count
+        //return places.count
+        return obligations.count
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -45,7 +67,8 @@ class createAlarmViewController: UIViewController, UIPickerViewDelegate, UIPicke
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        placeSelection = row
+        // placeSelection = row
+        obligationSelection = row
     }
     
     func calculateWakeup() {
